b1e47bb4b1be6263e4e6d9f4be134f7d30de7b3b
spirit <spirit@localhost>
2012-07-10 23:04:06 +0200

    Work on stdin instead.

--- guess_language/__init__.py
+++ guess_language/__init__.py
@@ -1,1 +1,3 @@
+"""Guess the natural language of a text
+"""
 from .guess_language import *
--- guess_language/__main__.py
+++ guess_language/__main__.py
@@ -1,10 +1,26 @@
+"""Command-line script for guess_language
+"""
+import argparse
+import locale
 import sys
-from .guess_language import guess_language
+
+import guess_language
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=guess_language.__doc__.strip())
+    parser.add_argument("--encoding", dest="encoding")
+    return parser.parse_args()
 
 
 def main():
-    for arg in sys.argv[1:]:
-        print(guess_language(arg))
+    args = parse_args()
+    encoding = (args.encoding if args.encoding
+                else sys.stdin.encoding if sys.stdin.isatty() else "utf-8")
+    with open(sys.stdin.fileno(), encoding=encoding) as f:
+        text = "\n".join(f.readlines())
+    print(guess_language.guess_language(text))
 
 
 if __name__ == "__main__":
