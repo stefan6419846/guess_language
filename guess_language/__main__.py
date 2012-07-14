aea4bd7d53c4440720d07e2788acbb137376f6ff
spirit <hiddenspirit@gmail.com>
2012-07-14 18:25:32 +0200

    file argument

--- guess_language/__main__.py
+++ guess_language/__main__.py
@@ -7,6 +7,8 @@ import sys
 
 import guess_language
 
+DEFAULT_ENCODING = "utf-8"
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -14,17 +16,22 @@ def parse_args():
         prog="{} -m {}".format(os.path.basename(sys.executable),
                                "guess_language")
     )
+    parser.add_argument("file", nargs="?", help="plain text file")
     parser.add_argument("--encoding", dest="encoding", help="input encoding")
-    parser.add_argument("--no-enchant", dest="use_enchant",
-                        action="store_false", help="don't use enchant")
+    parser.add_argument("--disable-enchant", dest="use_enchant",
+                        action="store_false", help="disable enchant")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    encoding = (args.encoding if args.encoding
-                else sys.stdin.encoding if sys.stdin.isatty() else "utf-8")
-    with open(sys.stdin.fileno(), encoding=encoding) as f:
+    if args.file:
+        file = args.file
+        encoding = args.encoding if args.encoding else DEFAULT_ENCODING
+    else:
+        file = sys.stdin.fileno()
+        encoding = args.encoding if args.encoding else sys.stdin.encoding
+    with open(file, encoding=encoding) as f:
         text = "\n".join(f.readlines())
     guess_language.USE_ENCHANT = args.use_enchant
     tag = guess_language.guess_language(text)
