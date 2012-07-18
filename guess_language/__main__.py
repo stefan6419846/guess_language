6a4c1f6c01178c21c1e50f31c721e5801073c7a7
spirit <hiddenspirit@gmail.com>
2012-07-18 21:39:53 +0200

    Use locale encoding when piping stdin.

--- guess_language/__main__.py
+++ guess_language/__main__.py
@@ -2,12 +2,11 @@
 """
 
 import argparse
+import locale
 import os
 import sys
 
 import guess_language
-
-DEFAULT_ENCODING = "utf-8"
 
 
 def parse_args():
@@ -16,26 +15,36 @@ def parse_args():
         prog="{} -m {}".format(os.path.basename(sys.executable),
                                "guess_language")
     )
-    parser.add_argument("file", nargs="?", help="plain text file")
-    parser.add_argument("--encoding", dest="encoding", help="input encoding")
+    parser.add_argument("file",
+                        help='plain text file or "-" for stdin')
+    parser.add_argument("--encoding",
+                        help="input encoding")
     parser.add_argument("--disable-enchant", dest="use_enchant",
-                        action="store_false", help="disable enchant")
+                        action="store_false",
+                        help="disable enchant")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    if args.file:
-        file = args.file
-        encoding = args.encoding if args.encoding else DEFAULT_ENCODING
-    else:
+
+    if args.file == "-":
         file = sys.stdin.fileno()
-        encoding = args.encoding if args.encoding else sys.stdin.encoding
+        encoding = args.encoding or (
+            sys.stdin.encoding if sys.stdin.isatty()
+            else locale.getpreferredencoding()
+        )
+    else:
+        file = args.file
+        encoding = args.encoding or "utf-8"
+
     with open(file, encoding=encoding) as f:
         text = "\n".join(f.readlines())
+
     guess_language.USE_ENCHANT = args.use_enchant
     tag = guess_language.guess_language(text)
     print(tag)
+
     return 0 if tag else 1
 
 
